4eda4e7772d23170d8ae9b3c67805bb3da563666
NeuroForLunch <neuro.for.lunch@gmail.com>
2021-05-31 15:18:21 -0700

    Update startup.sh

--- root/startup.sh
+++ root/startup.sh
@@ -44,31 +44,7 @@ if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
     chown -R $USER:$USER $HOME
 fi
 
-# nginx workers
-sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
 
-# nginx ssl
-if [ -n "$SSL_PORT" ] && [ -e "/etc/nginx/ssl/nginx.key" ]; then
-    echo "* enable SSL"
-	sed -i 's|#_SSL_PORT_#\(.*\)443\(.*\)|\1'$SSL_PORT'\2|' /etc/nginx/sites-enabled/default
-	sed -i 's|#_SSL_PORT_#||' /etc/nginx/sites-enabled/default
-fi
-
-# nginx http base authentication
-if [ -n "$HTTP_PASSWORD" ]; then
-    echo "* enable HTTP base authentication"
-    htpasswd -bc /etc/nginx/.htpasswd $USER $HTTP_PASSWORD
-	sed -i 's|#_HTTP_PASSWORD_#||' /etc/nginx/sites-enabled/default
-fi
-
-# dynamic prefix path renaming
-if [ -n "$RELATIVE_URL_ROOT" ]; then
-    echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
-	sed -i 's|#_RELATIVE_URL_ROOT_||' /etc/nginx/sites-enabled/default
-	sed -i 's|_RELATIVE_URL_ROOT_|'$RELATIVE_URL_ROOT'|' /etc/nginx/sites-enabled/default
-fi
-
-# clearup
 PASSWORD=
 HTTP_PASSWORD=
 

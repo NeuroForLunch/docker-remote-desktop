af9e2980910c25e0d80f1cc333e1a0607bec490b
NeuroForLunch <neuro.for.lunch@gmail.com>
2021-06-03 21:56:09 -0700

    Update startup.sh

--- root/startup.sh
+++ root/startup.sh
@@ -37,10 +37,9 @@ if [ "$USER" != "root" ]; then
 fi
 sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
 
-# home folder
+
 if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
     mkdir -p $HOME/.config/pcmanfm/LXDE/
-    chown -R $USER:$USER $HOME
 fi
 
 # cleanup
